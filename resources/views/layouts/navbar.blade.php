e74dd39ea3d43ce2d9fa9a4e9ed8511f23476105
MarkElezar <imarkelezar@gmail.com>
2025-11-19 16:35:48 +0800

    home responsive

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,11 +3,21 @@
         <img src="{{ asset('images/logo.png')}}" alt="Pagasa Multi-Purpose Cooperative" class="logo">
         <h2 class="name">PAGASA<br>MULTI-PURPOSE<br>COOPERATIVE</h2>
     </div>
-    <ul>
-        <li><a href="{{route('home')}}">HOME</a></li>
-        <li><a href="{{route('services')}}">SERVICES</a></li>
-        <li><a href="{{route('membership')}}">MEMBERSHIP</a></li>
-        <li><a href="{{route('about')}}">ABOUT US</a></li>
-        <li><a href="{{route('contact')}}">CONTACT US</a></li>
+
+    <!-- Hamburger Button (visible only on mobile/tablet) -->
+    <div class="hamburger-container">
+        <div class="hamburger" id="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </div>
+
+    <ul class="nav-menu" id="nav-menu">
+        <li><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
+        <li><a href="{{ route('services') }}" class="nav-link">SERVICES</a></li>
+        <li><a href="{{ route('membership') }}" class="nav-link">MEMBERSHIP</a></li>
+        <li><a href="{{ route('about') }}" class="nav-link">ABOUT US</a></li>
+        <li><a href="{{ route('contact') }}" class="nav-link">CONTACT US</a></li>
     </ul>
 </nav>
